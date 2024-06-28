f192e09ca1a9c7865fcb6a2cee1b1ebd77996938
Priti Umesh Jagtap <pritijagtap158@gmail.com>
2024-06-28 08:23:25 +0530

    Add files via upload

---
+++ Tempreture.java
@@ -0,0 +1,16 @@
+import java.util.scanner;
+
+public class temperature
+
+{
+
+public static void main (String argus[])
+{
+float Fahrenheit,celsius;
+   
+   celsuis=32
+   Fahrenheit=((celsuis*1.8+32;
+   System.out.printIn("Temperature in Fahrenheit is:"+Fahrenheit);
+
+}
+}
